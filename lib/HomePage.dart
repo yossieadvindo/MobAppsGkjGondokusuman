b1f758839be2d87d639b40146fe7218ef6558f21
Yossie Ruben Advindo <51661790+yossieadvindo@users.noreply.github.com>
2020-05-04 08:43:06 +0700

    masih home

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -4,17 +4,36 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-      ),
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Color(0xffDDE8EC),
+                border: Border.all(color: Color(0xffDDE8EC)),
+              ),
+              child: Padding(padding: EdgeInsets.all(30.0)),
+            ),
             Stack(
-              alignment: AlignmentDirectional.topCenter,
-              overflow: Overflow.visible,
               children: <Widget>[
-                backBgCover(),
+                ClipPath(
+                  clipper: CustomShapeClipper(),
+                  child: Container(
+                    height: 240.0,
+                    decoration: BoxDecoration(color: Color(0xffDDE8EC)),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      
+                    ],
+                  ),
+                ),
               ],
             )
           ],
@@ -22,16 +41,20 @@ class Home extends StatelessWidget {
       ),
     );
   }
+}
 
-  backBgCover() {
-    return Container(
-      height: 270,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-              colors: [Color(0xFFDDE8EC), Color(0xff7CB1C1)])),
-      child: Image(image: AssetImage('assets/gkjgondokusuman.png')),
-      );
+class CustomShapeClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+
+    path.lineTo(0.0, 390.0 - 200);
+    path.quadraticBezierTo(size.width / 2, 280, size.width, 390.0 - 200);
+    path.lineTo(size.width, 0.0);
+    path.close();
+    return path;
   }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
 }
